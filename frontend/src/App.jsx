216d303b16d61779d9c2b166d244eaad5e618051
Yagmur Duran <elifyagmurduran@gmail.com>
2026-03-24 14:21:25 +0100

    app initialization - establish all pages and scaffold routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import About from './pages/About'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
